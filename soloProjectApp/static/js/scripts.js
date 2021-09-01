db7cf2daa4877e0e9cc5417b963296a6054efdc5
paulsmalze <waffwegs@yahoo.com>
2021-09-01 10:40:32 -0500

    modified css scripts and index

--- soloProjectApp/static/js/scripts.js
+++ soloProjectApp/static/js/scripts.js
@@ -1,5 +1,18 @@
 
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
+// function myFunction() {
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+// }
+
+var css = document.querySelector('h5');
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+// console.log (css);
+color1.addEventListener('input', function(){
+    // console.log(color1.value);
+    body.style.background ="linear-gradient(to right," + color1.value + ", " + color2.value + ")";
+})
+color2.addEventListener('input', function(){
+    console.log(color1.value);
+})
